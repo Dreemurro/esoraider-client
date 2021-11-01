cb93fd8800351d61552e484abf12d61db91a11cd
Dreemurro <dr33murro@gmail.com>
2021-11-01 20:42:24 +0300

    Move uptimes display into separate component

---
+++ src/components/Uptimes.vue
@@ -0,0 +1,177 @@
+<template>
+  <template v-if="globalLoading">
+    <q-spinner size="50px" class="absolute-center" />
+  </template>
+
+  <template v-else>
+    <q-select
+      outlined
+      dense
+      options-dense
+      label="Target"
+      v-model="currentOption"
+      :options="options"
+      @update:model-value="changeTarget"
+      :loading="uptimesLoading"
+    />
+    <template v-if="Object.keys(currentAnalysis.skills).length !== 0">
+      <q-card bordered>
+        <q-expansion-item
+          default-opened
+          switch-toggle-side
+          expand-separator
+          label="Skills"
+        >
+          <template v-for="(skill, i) in currentAnalysis.skills" :key="i">
+            <skill :skill="skill" />
+          </template>
+        </q-expansion-item>
+      </q-card>
+    </template>
+
+    <template v-else>
+      <q-banner :class="$q.dark.isActive ? 'bg-yellow-8' : 'bg-yellow-3'">
+        <template v-slot:avatar>
+          <q-icon name="warning_amber" />
+        </template>
+        Character skills were not found. This log is probably broken or there is
+        nothing to track yet
+      </q-banner>
+    </template>
+
+    <template v-if="Object.keys(currentAnalysis.sets).length !== 0">
+      <q-card bordered>
+        <q-expansion-item
+          default-opened
+          switch-toggle-side
+          expand-separator
+          label="Sets"
+        >
+          <template v-for="(gearSet, i) in currentAnalysis.sets" :key="i">
+            <gear-set :gearSet="gearSet" />
+          </template>
+        </q-expansion-item>
+      </q-card>
+    </template>
+
+    <template v-else>
+      <q-banner :class="$q.dark.isActive ? 'bg-yellow-8' : 'bg-yellow-3'">
+        <template v-slot:avatar>
+          <q-icon name="warning_amber" />
+        </template>
+        Character sets were not found. This log is probably broken or there is
+        nothing to track yet
+      </q-banner>
+    </template>
+
+    <template v-if="Object.keys(currentAnalysis.glyphs).length !== 0">
+      <q-card bordered>
+        <q-expansion-item
+          default-opened
+          switch-toggle-side
+          expand-separator
+          label="Glyphs"
+        >
+          <template v-for="(glyph, i) in currentAnalysis.glyphs" :key="i">
+            <glyph :glyph="glyph" />
+          </template>
+        </q-expansion-item>
+      </q-card>
+    </template>
+
+    <template v-else>
+      <q-banner :class="$q.dark.isActive ? 'bg-yellow-8' : 'bg-yellow-3'">
+        <template v-slot:avatar>
+          <q-icon name="warning_amber" />
+        </template>
+        Character glyphs were not found. This log is probably broken or there is
+        nothing to track yet
+      </q-banner>
+    </template>
+  </template>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted, PropType } from 'vue';
+import { useRoute } from 'vue-router';
+import { Fight, AnalysisInfo } from 'components/models';
+import GearSet from 'components/GearSet.vue';
+import Glyph from 'components/Glyph.vue';
+import Skill from 'components/Skill.vue';
+export default defineComponent({
+  components: { GearSet, Glyph, Skill },
+  props: {
+    fight: {
+      type: Object as PropType<Fight>,
+      required: true,
+    },
+  },
+  emits: ['changeTarget'],
+  setup(props, { emit }) {
+    interface Option {
+      label: string;
+      value: number[];
+    }
+
+    const route = useRoute();
+
+    const globalLoading = ref(true);
+    const uptimesLoading = ref(false);
+
+    const currentAnalysis = ref({} as AnalysisInfo);
+    const currentOption = ref({} as Option);
+
+    const charId = Number(route.params.char);
+
+    let options = [] as Option[];
+
+    const populateTargets = () => {
+      options.push({
+        label: 'Overall',
+        value: [0],
+      });
+      for (let target of props.fight.chars[charId]['Overall'].report.targets)
+        options.push({
+          label: target.name,
+          value: target.id,
+        });
+    };
+
+    const changeTarget = (option: Option) => {
+      uptimesLoading.value = true;
+
+      if (!props.fight.chars[charId][option.label]) {
+        emit('changeTarget', option.value, option.label, finalize);
+      } else {
+        finalize(option.label);
+      }
+    };
+
+    const finalize = (option: string) => {
+      const report = props.fight.chars[charId][option].report;
+      if (report) currentAnalysis.value = report;
+
+      uptimesLoading.value = false;
+    };
+
+    onMounted(() => {
+      globalLoading.value = true;
+
+      populateTargets();
+      currentOption.value = options[0];
+      changeTarget(currentOption.value);
+
+      globalLoading.value = false;
+    });
+
+    return {
+      globalLoading,
+      uptimesLoading,
+      currentAnalysis,
+      currentOption,
+      options,
+      changeTarget,
+    };
+  },
+});
+</script>
--- src/pages/Analysis.vue
+++ src/pages/Analysis.vue
@@ -19,87 +19,19 @@
     </template>
 
     <template v-else>
-      <q-select
-        outlined
-        dense
-        options-dense
-        label="Target"
-        v-model="currentOption"
-        :options="options"
-        @update:model-value="changeTarget"
-        :loading="uptimesLoading"
-      />
-      <template v-if="Object.keys(currentAnalysis.skills).length !== 0">
-        <q-card bordered>
-          <q-expansion-item
-            default-opened
-            switch-toggle-side
-            label="Skills & Uptimes"
-          >
-            <template v-for="(skill, i) in currentAnalysis.skills" :key="i">
-              <skill :skill="skill" />
+      <q-card bordered>
+        <q-tabs v-model="currentTab" no-caps align="justify">
+          <q-tab name="uptimes" label="Uptimes" />
+        </q-tabs>
+        <q-separator />
+        <q-tab-panels v-model="currentTab" keep-alive>
+          <q-tab-panel name="uptimes" class="q-pa-md q-gutter-md">
+            <template v-if="Object.keys(currentFight).length !== 0">
+              <uptimes :fight="currentFight" @changeTarget="analysisRequest" />
             </template>
-          </q-expansion-item>
-        </q-card>
-      </template>
-
-      <template v-else>
-        <q-banner :class="$q.dark.isActive ? 'bg-yellow-8' : 'bg-yellow-3'">
-          <template v-slot:avatar>
-            <q-icon name="warning_amber" />
-          </template>
-          Character skills were not found. This log is probably broken or there
-          is nothing to track yet
-        </q-banner>
-      </template>
-
-      <template v-if="Object.keys(currentAnalysis.sets).length !== 0">
-        <q-card bordered>
-          <q-expansion-item
-            default-opened
-            switch-toggle-side
-            label="Sets & Uptimes"
-          >
-            <template v-for="(gearSet, i) in currentAnalysis.sets" :key="i">
-              <gear-set :gearSet="gearSet" />
-            </template>
-          </q-expansion-item>
-        </q-card>
-      </template>
-
-      <template v-else>
-        <q-banner :class="$q.dark.isActive ? 'bg-yellow-8' : 'bg-yellow-3'">
-          <template v-slot:avatar>
-            <q-icon name="warning_amber" />
-          </template>
-          Character sets were not found. This log is probably broken or there is
-          nothing to track yet
-        </q-banner>
-      </template>
-
-      <template v-if="Object.keys(currentAnalysis.glyphs).length !== 0">
-        <q-card bordered>
-          <q-expansion-item
-            default-opened
-            switch-toggle-side
-            label="Glyphs & Uptimes"
-          >
-            <template v-for="(glyph, i) in currentAnalysis.glyphs" :key="i">
-              <glyph :glyph="glyph" />
-            </template>
-          </q-expansion-item>
-        </q-card>
-      </template>
-
-      <template v-else>
-        <q-banner :class="$q.dark.isActive ? 'bg-yellow-8' : 'bg-yellow-3'">
-          <template v-slot:avatar>
-            <q-icon name="warning_amber" />
-          </template>
-          Character glyphs were not found. This log is probably broken or there
-          is nothing to track yet
-        </q-banner>
-      </template>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
     </template>
   </q-page>
 </template>
@@ -110,21 +42,14 @@ import { useRoute } from 'vue-router';
 import { useMeta } from 'quasar';
 import { useStore } from 'src/store';
 import { AxiosError } from 'axios';
-import { Fight, AnalysisInfo } from 'components/models';
+import { Fight } from 'components/models';
 import ErrorBanner from 'components/ErrorBanner.vue';
-import GearSet from 'components/GearSet.vue';
-import Glyph from 'components/Glyph.vue';
-import Skill from 'components/Skill.vue';
+import Uptimes from 'components/Uptimes.vue';
 
 export default defineComponent({
   name: 'analysis',
-  components: { Skill, GearSet, ErrorBanner, Glyph },
+  components: { ErrorBanner, Uptimes },
   setup() {
-    interface Option {
-      label: string;
-      value: number[];
-    }
-
     const $store = useStore();
     const route = useRoute();
 
@@ -132,17 +57,13 @@ export default defineComponent({
     const error = ref({} as AxiosError);
 
     const globalLoading = ref(true);
-    const uptimesLoading = ref(false);
 
-    const currentAnalysis = ref({} as AnalysisInfo);
-    const currentOption = ref({} as Option);
+    const currentTab = ref('uptimes');
+    const currentFight = ref({} as Fight);
 
     const logCode = <string>route.params.log;
     const fightId = Number(route.params.fight);
     const charId = Number(route.params.char);
-
-    let options = [] as Option[];
-    let currentFight = {} as Fight;
 
     useMeta(() => {
       return {
@@ -151,7 +72,11 @@ export default defineComponent({
       };
     });
 
-    const analysisRequest = async (targetId?: number[]) => {
+    const analysisRequest = async (
+      targetId?: number,
+      targetName?: string,
+      done?: (targetName: string) => void
+    ) => {
       await $store.dispatch('eso/requestAnalysis', {
         log: logCode,
         fight: fightId,
@@ -161,30 +86,8 @@ export default defineComponent({
 
       error.value = $store.state.eso.error;
       if (Object.keys(error.value).length !== 0) title.value = 'Error';
-    };
 
-    const populateTargets = () => {
-      options.push({
-        label: 'Overall',
-        value: [0],
-      });
-      for (let target of currentFight.chars[charId]['Overall'].report.targets)
-        options.push({
-          label: target.name,
-          value: target.id,
-        });
-    };
-
-    const changeTarget = async (option: Option) => {
-      uptimesLoading.value = true;
-
-      if (!currentFight.chars[charId][option.label]) {
-        await analysisRequest(option.value);
-      }
-      const report = currentFight.chars[charId][option.label].report;
-      if (report) currentAnalysis.value = report;
-
-      uptimesLoading.value = false;
+      if (targetId && targetName && done) done(targetName);
     };
 
     onMounted(async () => {
@@ -197,24 +100,19 @@ export default defineComponent({
         return;
       }
 
-      currentFight = $store.state.eso.logs[logCode].fights[fightId];
+      currentFight.value = $store.state.eso.logs[logCode].fights[fightId];
+      const report = currentFight.value.chars[charId]['Overall'].report;
 
-      populateTargets();
-      currentOption.value = options[0];
-      await changeTarget(currentOption.value);
-
-      title.value = currentAnalysis.value.char.name;
+      title.value = report.char.name;
       globalLoading.value = false;
     });
 
     return {
       globalLoading,
-      uptimesLoading,
-      currentAnalysis,
-      currentOption,
-      options,
       error,
-      changeTarget,
+      currentTab,
+      currentFight,
+      analysisRequest,
     };
   },
 });
